89945238aabe65411b8aac07c5d346c9212230de
Veldermon-rbg <burtjayden3@gmail.com>
2024-12-23 12:42:38 +1300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,7 +1,4 @@
 @echo off
-REM Navigate to the directory where free-games-claimer is located
-cd /d "C:\Users\Burts\Desktop\code\free-games-claimer_automation-main\free-games-claimer_automation-main\Windows"
-
 REM Start Epic Games claim
 start "" /B cmd /c node epic-games
 REM Wait for 10 seconds
